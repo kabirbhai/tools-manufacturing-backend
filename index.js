54ff65c0608b7fe0793bea9633cbe33cba41860f
kabir <makkobir79@gmail.com>
2022-07-12 14:07:53 +0600

    connected server to the mongoDB

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MIDDLEWARE
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`The app is listening on port: ${port}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(` The app is listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.dir(error);
+  });
